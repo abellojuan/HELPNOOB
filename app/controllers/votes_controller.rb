b572d583d12f7b8a7911437996fbc20d90b912ee
Juan Abello <abellojuan@hotmail.com>
2013-01-10 23:32:07 -0500

    problem in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
   
   def create
-    post = Post.find(params[:post_id])
+    post = Post.where(params[:post_id])
     post.votes.create(direction: params[:direction])
     redirect_to root_path
   end
