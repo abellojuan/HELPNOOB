ac670d365acc183e2c0f24149a5996adcac1c4aa
Juan Abello <abellojuan@hotmail.com>
2013-03-08 15:08:52 -0500

    solving votes bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,7 @@ class VotesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    post.votes.create(direction: params[:direction])
+    @post.votes.create(direction: params[:direction])
     redirect_to root_path
   end
 end
